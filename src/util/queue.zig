5c2cf1b6cc4f81477c4058932863cce9af9cc0b5
Khitiara <robotbrainify@gmail.com>
2024-05-04 17:23:12 -0400

    relicense the queues file

--- src/util/queue.zig
+++ src/util/queue.zig
@@ -1,5 +1,22 @@
-// derived from https://github.com/FlorenceOS/Florence/blob/aaa5a9e568197ad24780ec9adb421217530d4466/lib/containers/queue.zig
-// which was released under CC0
+// THIS FILE ALONE IS LICENSED UNDER THE BSD 0-CLAUSE LICENSE
+//
+// Zero-Clause BSD
+// =============
+//
+// Permission to use, copy, modify, and/or distribute this software for
+// any purpose with or without fee is hereby granted.
+//
+// THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+// FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+// DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+// AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+// OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+// these implementations are derived from N00byEdge's work in
+// https://github.com/FlorenceOS/Florence/blob/aaa5a9e568197ad24780ec9adb421217530d4466/lib/containers/queue.zig
+// which was released under the BSD 0-clause
 
 const std = @import("std");
 const CopyPtrAttrs = @import("util.zig").CopyPtrAttrs;
