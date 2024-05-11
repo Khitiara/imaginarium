338631eda16b81bd8d24b3ed1f1c02ac7c8f058f
Khitiara <robotbrainify@gmail.com>
2024-05-11 15:26:08 -0400

    reorganize main & extract debug/print code

---
+++ src/krnl/debug.zig
@@ -0,0 +1,103 @@
+const std = @import("std");
+const hal = @import("hal");
+const arch = hal.arch;
+const puts = arch.puts;
+
+pub const SerialWriter = struct {
+    const WriteError = error{};
+    pub const Writer = std.io.GenericWriter(*const anyopaque, error{}, typeErasedWriteFn);
+
+    fn typeErasedWriteFn(context: *const anyopaque, bytes: []const u8) error{}!usize {
+        _ = context;
+        puts(bytes);
+        return bytes.len;
+    }
+
+    pub fn writer() Writer {
+        return .{ .context = undefined };
+    }
+};
+
+const log = std.log.default;
+
+pub fn print_stack_trace(logger: anytype, trace: *std.builtin.StackTrace) void {
+    var i: usize = 0;
+    var frame_index: usize = 0;
+    var frames_left: usize = @min(trace.index, trace.instruction_addresses.len);
+    while (frames_left != 0) : ({
+        frames_left -= 1;
+        frame_index = (frame_index + 1) % trace.instruction_addresses.len;
+        i += 1;
+    }) {
+        const return_address = trace.instruction_addresses[frame_index];
+        logger.debug("   at   {d: <4}: {x:0>16}", .{ i, return_address });
+    }
+}
+
+pub fn dump_stack_trace(logger: anytype, ret_addr: ?usize) void {
+    logger.debug("current stack trace: ", .{});
+    var addrs: [16]usize = undefined;
+    var trace: std.builtin.StackTrace = .{
+        .instruction_addresses = &addrs,
+        .index = 0,
+    };
+    std.debug.captureStackTrace(ret_addr orelse @returnAddress(), &trace);
+    print_stack_trace(logger, &trace);
+}
+
+pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, ret_addr: ?usize) noreturn {
+    log.err("PANIC {s}, RETURN={X:0>16}; error return trace:", .{ msg, ret_addr orelse 0 });
+    if (error_return_trace) |stk| {
+        print_stack_trace(log, stk);
+    } else {
+        log.err("    ---", .{});
+    }
+    dump_stack_trace(log, ret_addr);
+    while (true) {
+        @breakpoint();
+    }
+}
+
+pub fn dump_hex(bytes: []const u8) !void {
+    const writer = SerialWriter.writer();
+    var chunks = std.mem.window(u8, bytes, 16, 16);
+    while (chunks.next()) |window| {
+        // 1. Print the address.
+        const address = (@intFromPtr(bytes.ptr) + 0x10 * (chunks.index orelse 0) / 16) - 0x10;
+        // We print the address in lowercase and the bytes in uppercase hexadecimal to distinguish them more.
+        // Also, make sure all lines are aligned by padding the address.
+        try writer.print("{x:0>[1]}  ", .{ address, @sizeOf(usize) * 2 });
+
+        // 2. Print the bytes.
+        for (window, 0..) |byte, index| {
+            try writer.print("{X:0>2} ", .{byte});
+            if (index == 7) try writer.writeByte(' ');
+        }
+        try writer.writeByte(' ');
+        if (window.len < 16) {
+            var missing_columns = (16 - window.len) * 3;
+            if (window.len < 8) missing_columns += 1;
+            try writer.writeByteNTimes(' ', missing_columns);
+        }
+
+        // 3. Print the characters.
+        for (window) |byte| {
+            if (std.ascii.isPrint(byte)) {
+                try writer.writeByte(byte);
+            } else {
+                // Related: https://github.com/ziglang/zig/issues/7600
+
+                // Let's print some common control codes as graphical Unicode symbols.
+                // We don't want to do this for all control codes because most control codes apart from
+                // the ones that Zig has escape sequences for are likely not very useful to print as symbols.
+                switch (byte) {
+                    '\n' => try writer.writeAll("␊"),
+                    '\r' => try writer.writeAll("␍"),
+                    '\t' => try writer.writeAll("␉"),
+                    else => try writer.writeByte('.'),
+                }
+            }
+        }
+        try writer.writeByte('\n');
+    }
+}
--- src/krnl/main.zig
+++ src/krnl/main.zig
@@ -13,20 +13,41 @@ const puts = arch.puts;
 
 const fb = @import("framebuffer.zig");
 const font_rendering = @import("font_rendering.zig");
+const debug = @import("debug.zig");
+const smp = @import("smp.zig");
 
-const SerialWriter = struct {
-    const WriteError = error{};
-    pub const Writer = std.io.GenericWriter(*const anyopaque, error{}, typeErasedWriteFn);
+const log = std.log.default;
 
-    fn typeErasedWriteFn(context: *const anyopaque, bytes: []const u8) error{}!usize {
-        _ = context;
-        puts(bytes);
-        return bytes.len;
+fn logFn(
+    comptime message_level: std.log.Level,
+    comptime scope: @TypeOf(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    puts(util.upper_string_comptime(message_level.asText()));
+    if (scope != .default) {
+        puts(" (");
+        puts(util.lower_string_comptime(@tagName(scope)));
+        puts(")");
     }
-
-    pub fn writer() Writer {
-        return .{ .context = undefined };
+    puts(": ");
+    debug.SerialWriter.writer().print(format, args) catch unreachable;
+    if (format.len == 0 or format[format.len - 1] != '\n') {
+        puts("\n");
     }
+}
+
+pub const os = struct {
+    pub const heap = struct {
+        pub const page_allocator = arch.x86_64.vmm.raw_page_allocator.allocator();
+    };
+    pub const panic = debug.panic;
+};
+
+pub const std_options: std.Options = .{
+    .logFn = logFn,
+    // .crypto_always_getrandom = true,
+    .cryptoRandomSeed = arch.x86_64.rand.fill_secure,
 };
 
 /// the true entry point is __kstart and is exported by global asm in `hal/arch/{arch}/init.zig`
@@ -49,83 +70,6 @@ export fn __kstart2(ldr_info: *bootelf.BootelfData) callconv(arch.cc) noreturn {
         asm volatile ("hlt");
     }
 }
-
-fn logFn(
-    comptime message_level: std.log.Level,
-    comptime scope: @TypeOf(.enum_literal),
-    comptime format: []const u8,
-    args: anytype,
-) void {
-    puts(util.upper_string_comptime(message_level.asText()));
-    if (scope != .default) {
-        puts(" (");
-        puts(util.lower_string_comptime(@tagName(scope)));
-        puts(")");
-    }
-    puts(": ");
-    SerialWriter.writer().print(format, args) catch unreachable;
-    if (format.len == 0 or format[format.len - 1] != '\n') {
-        puts("\n");
-    }
-}
-
-pub const os = struct {
-    pub const heap = struct {
-        pub const page_allocator = arch.x86_64.vmm.raw_page_allocator.allocator();
-    };
-};
-
-pub const std_options: std.Options = .{
-    .logFn = logFn,
-    // .crypto_always_getrandom = true,
-    .cryptoRandomSeed = arch.x86_64.rand.fill_secure,
-};
-
-const log = std.log.default;
-
-pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, ret_addr: ?usize) noreturn {
-    log.err("PANIC {s}, RETURN={X:0>16}; error return trace:", .{ msg, ret_addr orelse 0 });
-    if (error_return_trace) |stk| {
-        var i: usize = 0;
-        var frame_index: usize = 0;
-        var frames_left: usize = @min(stk.index, stk.instruction_addresses.len);
-        while (frames_left != 0) : ({
-            frames_left -= 1;
-            frame_index = (frame_index + 1) % stk.instruction_addresses.len;
-            i += 1;
-        }) {
-            const return_address = stk.instruction_addresses[frame_index];
-            log.err("   at   {d: <4}: {x:0>16}", .{ i, return_address });
-        }
-    } else {
-        log.err("    ---", .{});
-    }
-    log.err("current stack trace: ", .{});
-    var addrs: [16]usize = undefined;
-    var trace: std.builtin.StackTrace = .{
-        .instruction_addresses = &addrs,
-        .index = 0,
-    };
-    std.debug.captureStackTrace(ret_addr orelse @returnAddress(), &trace);
-    {
-        var i: usize = 0;
-        var frame_index: usize = 0;
-        var frames_left: usize = @min(trace.index, trace.instruction_addresses.len);
-        while (frames_left != 0) : ({
-            frames_left -= 1;
-            frame_index = (frame_index + 1) % trace.instruction_addresses.len;
-            i += 1;
-        }) {
-            const return_address = trace.instruction_addresses[frame_index];
-            log.err("   at   {d: <4}: {x:0>16}", .{ i, return_address });
-        }
-    }
-    while (true) {
-        @breakpoint();
-    }
-}
-
-const smp = @import("smp.zig");
 
 fn main(ldr_info: *bootelf.BootelfData) !void {
     const bootelf_magic_check = ldr_info.magic == bootelf.magic;
@@ -170,51 +114,8 @@ fn main(ldr_info: *bootelf.BootelfData) !void {
 
     log.debug("ap_trampoline: {*} (len {X})", .{ ap_trampoline_start, ap_trampoline_length });
 
-    try dump_hex(ap_trampoline_start[0..ap_trampoline_length]);
-}
-
-pub fn dump_hex(bytes: []const u8) !void {
-    const writer = SerialWriter.writer();
-    var chunks = std.mem.window(u8, bytes, 16, 16);
-    while (chunks.next()) |window| {
-        // 1. Print the address.
-        const address = (@intFromPtr(bytes.ptr) + 0x10 * (chunks.index orelse 0) / 16) - 0x10;
-        // We print the address in lowercase and the bytes in uppercase hexadecimal to distinguish them more.
-        // Also, make sure all lines are aligned by padding the address.
-        try writer.print("{x:0>[1]}  ", .{ address, @sizeOf(usize) * 2 });
-
-        // 2. Print the bytes.
-        for (window, 0..) |byte, index| {
-            try writer.print("{X:0>2} ", .{byte});
-            if (index == 7) try writer.writeByte(' ');
-        }
-        try writer.writeByte(' ');
-        if (window.len < 16) {
-            var missing_columns = (16 - window.len) * 3;
-            if (window.len < 8) missing_columns += 1;
-            try writer.writeByteNTimes(' ', missing_columns);
-        }
-
-        // 3. Print the characters.
-        for (window) |byte| {
-            if (std.ascii.isPrint(byte)) {
-                try writer.writeByte(byte);
-            } else {
-                // Related: https://github.com/ziglang/zig/issues/7600
-
-                // Let's print some common control codes as graphical Unicode symbols.
-                // We don't want to do this for all control codes because most control codes apart from
-                // the ones that Zig has escape sequences for are likely not very useful to print as symbols.
-                switch (byte) {
-                    '\n' => try writer.writeAll("␊"),
-                    '\r' => try writer.writeAll("␍"),
-                    '\t' => try writer.writeAll("␉"),
-                    else => try writer.writeByte('.'),
-                }
-            }
-        }
-        try writer.writeByte('\n');
-    }
+    try debug.dump_hex(ap_trampoline_start[0..ap_trampoline_length]);
+    debug.dump_stack_trace(log, null);
 }
 
 test {
