cc720ab00acc60406a4ecf3843cdca9ae38da911
Khitiara <robotbrainify@gmail.com>
2025-01-27 14:14:03 -0500

    high irql spin lock mutex shim for GPA

---
+++ src/krnl/std_shims/spin_lock_mutex_impls.zig
@@ -0,0 +1,15 @@
+const hal = @import("../hal/hal.zig");
+const SpinLock = hal.SpinLock;
+
+pub const HighSpinLockMutex = struct {
+    spin_lock: SpinLock,
+    saved_state: bool,
+
+    pub fn lock(self: *HighSpinLockMutex) void {
+        self.saved_state = self.spin_lock.lock_cli();
+    }
+
+    pub fn unlock(self: *HighSpinLockMutex) void {
+        self.spin_lock.unlock_sti(self.saved_state);
+    }
+};
