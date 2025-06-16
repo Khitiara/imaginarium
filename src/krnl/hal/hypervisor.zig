f8e0869e4a647d6f9eccd11b7ed03f12d12bf0ad
Khitiara <robotbrainify@gmail.com>
2025-06-16 14:56:23 -0400

    better hypervisor info setup

--- src/krnl/hal/hypervisor.zig
+++ src/krnl/hal/hypervisor.zig
@@ -7,11 +7,49 @@ pub var present: bool = false;
 var sub_features: cpuid.SvmSubFeatures = undefined;
 var asid_count: u32 = undefined;
 
+pub const KnownHypervisor = union(enum) {
+    kvm: struct {
+        max_hypervisor_leaf: u32,
+    },
+    unknown: [12]u8,
+    none,
+
+    pub fn format(
+        self: @This(),
+        comptime fmt: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        if (!std.mem.eql(u8, "s", fmt)) {
+            return error.InvalidFormat;
+        }
+        switch (self) {
+            .kvm => |kvm| {
+                try writer.print("KVM Hypervisor, max hypervisor cpuid leaf: {x:0>8}", .{kvm.max_hypervisor_leaf});
+            },
+            .unknown => |un| {
+                try writer.print("Unknown hypervisor, vendor \"{s}\"", .{un});
+            },
+            .none => {
+                try writer.print("No hypervisor", .{});
+            },
+        }
+    }
+};
+
+pub var hypervisor: KnownHypervisor = .none;
+
 pub fn init() void {
     const info: SvmInfo = cpuid.cpuid(.svm, {});
     const tfmsf = cpuid.cpuid(.type_fam_model_stepping_features, {});
     present = @import("config").force_hypervisor or tfmsf.features2.hv or info.revision_presence.present;
     sub_features = info.sub_features;
     asid_count = info.asid_count;
-}
 
+    const hypervisor_info = cpuid.cpuid(.hypervisor_vendor, {});
+    if (std.mem.eql(u8, "KVMKVMKVM" ++ .{ 0, 0, 0 }, &hypervisor_info.vendor_id)) {
+        hypervisor = .{ .kvm = .{ .max_hypervisor_leaf = hypervisor_info.eax } };
+    }
+
+    log.info("{s}", .{hypervisor});
+}
